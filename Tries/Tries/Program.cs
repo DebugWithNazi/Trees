c6ce5888a36e12aa8bb4d1a66cd5743171d78bbd
Nazia Khan <naziaramzankhan@gmail.com>
2024-12-15 22:24:53 +0500

    tries added

---
+++ Tries/Tries/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tries;
+
+Trie trie = new Trie();
+trie.Insert("apple");
+trie.Insert("app");
+trie.Insert("banana");
+
+Console.WriteLine(trie.Search("apple"));  
+Console.WriteLine(trie.Search("app"));     
+Console.WriteLine(trie.Search("appl"));  
+
+Console.WriteLine(trie.StartsWith("app")); 
+Console.WriteLine(trie.StartsWith("ban")); 
+Console.WriteLine(trie.StartsWith("bat"));
---
+++ Tries/Tries/Trie.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tries
+{
+    public class TrieNode
+    {
+        public Dictionary<char, TrieNode> Children {  get; set; }
+        public bool IsEndOfWord {  get; set; }
+        public TrieNode() 
+        {
+            Children = new Dictionary<char, TrieNode>();
+            IsEndOfWord = false;
+        }
+    }
+    public class Trie
+    {
+        private TrieNode root;
+        public Trie()
+        {
+            root = new TrieNode();
+        }
+
+        public void Insert(string word)
+        {
+            TrieNode current = root;
+            foreach(char ch in word)
+            {
+                if (!current.Children.ContainsKey(ch))
+                {
+                    current.Children[ch] = new TrieNode();
+                }
+                current = current.Children[ch];
+            }
+            current.IsEndOfWord = true;
+        }
+
+        public bool Search(string word)
+        {
+            TrieNode current = root;
+            foreach(char ch in word)
+            {
+                if (!current.Children.ContainsKey(ch))
+                {
+                    return false;
+                }
+                current = current.Children[ch];
+            }
+            return current.IsEndOfWord;
+        }
+
+        public bool StartsWith(string prefix)
+        {
+            TrieNode current = root;
+            foreach(char ch in prefix)
+            {
+                if (!current.Children.ContainsKey(ch))
+                {
+                    return false;
+                }
+                current = current.Children[ch];
+            }
+            return true;
+        }
+    }
+}
