a25901737b6ae2ffcb45a059542eda5f9f09f640
Nazia Khan <naziaramzankhan@gmail.com>
2024-12-20 16:30:58 +0500

    Graph implemented

---
+++ Graphs/Graphs/Graph.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Graphs
+{
+    public class Graph
+    {
+        private Dictionary<int, List<int>> adjacancyList;
+        public Graph()
+        {
+            adjacancyList = new Dictionary<int, List<int>>();
+        }
+        public void AddVertex(int vertex)
+        {
+            if (!adjacancyList.ContainsKey(vertex))
+            {
+                adjacancyList[vertex] = new List<int>();
+            }
+        }
+        public void AddEdge(int vertex1, int vertex2)
+        {
+            AddVertex(vertex1);
+            AddVertex(vertex2);
+            adjacancyList[vertex1].Add(vertex2);
+            adjacancyList[vertex2].Add(vertex1);
+        }
+
+        public void BFS(int startVertex)
+        {
+            HashSet<int> visited = new HashSet<int>();
+            Queue<int> queue = new Queue<int>();
+            visited.Add(startVertex);
+            queue.Enqueue(startVertex);
+
+            while (queue.Count > 0)
+            {
+                int vertex = queue.Dequeue();
+                Console.Write(vertex + " ");
+                foreach (var neighbor in adjacancyList[vertex])
+                {
+                    if (!visited.Contains(neighbor))
+                    {
+                        visited.Add(neighbor);
+                        queue.Enqueue(neighbor);
+                    }
+                }
+            }
+        }
+        public void DfS(int startVertex)
+        {
+            HashSet<int> visited = new HashSet<int>();
+            DFSHelper(startVertex,visited);
+            Console.WriteLine();
+        }
+
+        public void DFSHelper(int startVertex, HashSet<int> visited)
+        {
+            visited.Add(startVertex);
+            Console.Write(startVertex + " ");
+            foreach(var neighbor in adjacancyList[startVertex])
+            {
+                if (!visited.Contains(neighbor))
+                {
+                    DFSHelper(neighbor, visited);
+                }
+            }
+        }
+    }
+}
---
+++ Graphs/Graphs/Program.cs
@@ -0,0 +1,11 @@
+﻿using Graphs;
+
+Graph graph = new Graph();
+graph.AddEdge(0, 1);
+graph.AddEdge(0, 2);
+graph.AddEdge(1, 3);
+graph.AddEdge(1, 4);
+graph.AddEdge(2, 5);
+graph.AddEdge(2, 6);
+
+graph.DfS(0);
